f46cdc58bed11d2dc47373eae872c887d4a37a78
Nicholas Bollweg <nick.bollweg@gmail.com>
2023-05-31 09:02:09 -0500

    fix licenses

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,9 +5,10 @@ set -eux
 export TMPDIR="$( pwd )/tmp"
 mkdir -p "${TMPDIR}"
 
-
 export GOPATH="$( pwd )"
 export GOFLAGS="-buildmode=pie -trimpath -mod=vendor -modcacherw -ldflags=-linkmode=external"
+export GO111MODULE=on
+
 # consumed by `scripts/binary` below to avoid wrangling versioned flags
 export VERSION="${PKG_VERSION}"
 export DATE="$(date -u '+%Y-%m-%d_%I:%M:%S%p')"
@@ -16,17 +17,14 @@ module='github.com/traefik/traefik'
 
 cd "src/${module}"
 
-go mod vendor
+go get .
 
 bash script/binary
 
-mkdir -p ${PREFIX}/bin
+mkdir -p "${PREFIX}/bin"
 
-cp dist/traefik ${PREFIX}/bin
+cp dist/traefik "${PREFIX}/bin"
 
 go-licenses save \
     "." \
-    --save_path "${SRC_DIR}/library_licenses/" \
-    2>&1 \
-    | tee "${SRC_DIR}/go-licenses.log" \
-    || echo "some errors: captured in go-licenses.log"
+    --save_path "${SRC_DIR}/library_licenses/"
